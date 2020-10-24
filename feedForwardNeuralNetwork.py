39dc69fcdf2d34549e38924248bb8a3cf6027abd
wazedrifat <34921453+wazedrifat@users.noreply.github.com>
2020-10-24 18:41:23 +0600

    .

--- feedForwardNeuralNetwork.py
+++ feedForwardNeuralNetwork.py
@@ -1,10 +1,12 @@
 import pandas as pd
 import numpy as np
+import tensorflow as tf
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
-from tensorflow.keras.models import sequential
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
 
-data = pd.read_csv('finalwithNull.csv')
+data = pd.read_csv('final.csv')
 
 x = data.drop('Class', axis = 1)
 y = data['Class']
@@ -14,3 +16,20 @@ scaler = MinMaxScaler()
 scaler.fit(x_train)
 x_train = scaler.transform(x_train) 
 x_test = scaler.transform(x_test)
+
+model = Sequential()
+for i in range (50):
+	model.add(Dense(50, activation='relu'))
+model.add(Dense(1))
+
+model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['accuracy'])
+model.fit(x=x_train, y=y_train, epochs=500)
+
+model.evaluate(x_test, y_test, verbose=0)
+
+pred = model.predict(x_test)
+
+print(pred)
+
+
+
